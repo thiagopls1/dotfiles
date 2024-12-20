1625085016eb204c8d1887fcc1bf8a4f2fa490c3
thiagopls1 <thiagopls1@hotmail.com>
2024-12-20 10:21:15 -0300

    Setup lsp-format

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -1,5 +1,13 @@
+-- LSP Format setup
+local lsp_format = require "lsp-format"
+lsp_format.setup {}
+
 -- Languages Support
-require"lspconfig".pyright.setup{
+local lspconf = require "lspconfig"
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+lspconf.pyright.setup {
 	settings = {
 		pyright = {
 			strictParameterNoneValue = false,
@@ -9,17 +17,14 @@ require"lspconfig".pyright.setup{
 			typeCheckingMode = 'off',
 			analyzeUnannotatedFunctions = false
 		}
-	}
+	},
+	on_attach = lsp_format.on_attach,
 }
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-local lspconf = require"lspconfig"
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-lspconf.csharp_ls.setup{}
-lspconf.ts_ls.setup{}
-lspconf.angularls.setup{}
-lspconf.lua_ls.setup{
+lspconf.csharp_ls.setup { on_attach = lsp_format.on_attach, }
+lspconf.ts_ls.setup { on_attach = lsp_format.on_attach, }
+lspconf.angularls.setup { on_attach = lsp_format.on_attach, }
+lspconf.lua_ls.setup {
 	settings = {
 		Lua = {
 			diagnostics = {
@@ -27,28 +32,36 @@ lspconf.lua_ls.setup{
 			},
 		},
 	},
+	on_attach = lsp_format.on_attach,
 }
 
-lspconf.cssls.setup{
+lspconf.cssls.setup {
 	capabilities = capabilities,
+	on_attach = lsp_format.on_attach,
 }
 
-lspconf.html.setup{
+lspconf.html.setup {
 	capabilities = capabilities,
+	on_attach = lsp_format.on_attach,
 }
 
-lspconf.azure_pipelines_ls.setup{
+lspconf.azure_pipelines_ls.setup {
 	settings = {
 		yaml = {
 			schemas = {
 				["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] = {
 					"/azure-pipeline*.y*l",
-          "/*.azure*",
-          "Azure-Pipelines/**/*.y*l",
-          "Pipelines/*.y*l",
+					"/*.azure*",
+					"Azure-Pipelines/**/*.y*l",
+					"Pipelines/*.y*l",
 				},
 			},
 		},
 	},
+	on_attach = lsp_format.on_attach,
 }
 
+-- Prettier setup
+vim.g['prettier#exec_cmd_async'] = 1
+vim.g['prettier#autoformat'] = 1
+vim.cmd "au BufWritePre *.js,*.jsx,*.ts,*.tsx,*.html,*.css Prettier"
