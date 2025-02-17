afa65091d5703895c711605d6c63b726cc5f14ab
thiagopls1 <thiagopls1@hotmail.com>
2025-02-17 14:38:25 -0300

    Update `nvim` with `snacks.nvim`

--- .config/nvim/plugin/telescope.lua
+++ .config/nvim/plugin/telescope.lua
@@ -1,37 +1,37 @@
-local telescope = require "telescope"
-local builtin = require "telescope.builtin"
-local actions = require "telescope.actions"
+-- local telescope = require "telescope"
+-- local builtin = require "telescope.builtin"
+-- local actions = require "telescope.actions"
+--
+-- telescope.load_extension "emoji"
+-- telescope.setup {
+-- 	pickers = {
+-- 		find_files = {
+-- 			file_ignore_patterns = {
+-- 				"node_modules",
+-- 				"__pycache__",
+-- 				".pytest_cache",
+-- 				"htmlcov",
+-- 				".venv",
+-- 				".git/.*",
+-- 				"dist",
+-- 				".next",
+-- 				"bin",
+-- 				"obj",
+-- 			},
+-- 			hidden = true,
+-- 		},
+-- 		buffers = {
+-- 			mappings = {
+-- 				i = {
+-- 					["<c-s-d>"] = actions.delete_buffer,
+-- 				}
+-- 			}
+-- 		}
+-- 	}
+-- }
 
-telescope.load_extension "emoji"
-telescope.setup {
-	pickers = {
-		find_files = {
-			file_ignore_patterns = {
-				"node_modules",
-				"__pycache__",
-				".pytest_cache",
-				"htmlcov",
-				".venv",
-				".git/.*",
-				"dist",
-				".next",
-				"bin",
-				"obj",
-			},
-			hidden = true,
-		},
-		buffers = {
-			mappings = {
-				i = {
-					["<c-s-d>"] = actions.delete_buffer,
-				}
-			}
-		}
-	}
-}
-
-vim.keymap.set("n", "<leader>p", builtin.find_files, { desc = "Telescope find files" })
-vim.keymap.set("n", "<leader>r", builtin.lsp_references, { desc = "Telescope peek references" })
-vim.keymap.set("n", "<leader>f", builtin.live_grep, { desc = "Search in files" })
-vim.keymap.set("n", "<leader>B", builtin.buffers, { desc = "Buffers" })
-vim.keymap.set("n", "<leader>.", "<Cmd>Telescope emoji<CR>", { desc = "Telesope emojis" })
+-- vim.keymap.set("n", "<leader>p", builtin.find_files, { desc = "Telescope find files" })
+-- vim.keymap.set("n", "<leader>r", builtin.lsp_references, { desc = "Telescope peek references" })
+-- vim.keymap.set("n", "<leader>f", builtin.live_grep, { desc = "Search in files" })
+-- vim.keymap.set("n", "<leader>B", builtin.buffers, { desc = "Buffers" })
+-- vim.keymap.set("n", "<leader>.", "<Cmd>Telescope emoji<CR>", { desc = "Telesope emojis" })
