1a33ad56340f4dbc99fa848408a21f98ee14e763
thiagopls1 <thiagopls1@hotmail.com>
2025-07-07 10:00:23 -0300

    Add more friendly messages to pacman last update script

--- scripts/get_pacman_last_update_date.sh
+++ scripts/get_pacman_last_update_date.sh
@@ -1,7 +1,7 @@
 LOG_FILE="/var/log/pacman.log"
 
 if [ ! -f $LOG_FILE ]; then
-	echo "Erro: Arquivo de log do pacman não encontrado ($LOG_FILE)."
+	echo "pacman not found"
   exit 1
 fi
 
@@ -9,7 +9,7 @@ DATE_REGEX='[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}T[0-9]\{2\}\:[0-9]\{2\}\:[0-9]\{2\}\
 LAST_DATE=$(grep -E "(pacman -Syu|full system upgrade)" $LOG_FILE | tail -n 1 | sed -n "s/.*\($DATE_REGEX\).*/\1/p")
 
 if [ -z "$LAST_DATE" ]; then
-  echo "Nenhum registro de 'pacman -Syu' encontrado no log."
+  echo "No updates found"
 else
 	echo $(date -d $LAST_DATE '+%a %b %d %H:%m:%S')
 fi
