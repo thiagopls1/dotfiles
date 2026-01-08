8c5aa668b582c7ad3c3c67581f2b1dc8de7f6539
thiagopls1 <thiagopls1@hotmail.com>
2026-01-08 15:03:38 -0300

    Add `.rcloneignore` for backup folders

--- scripts/sync_rclone.sh
+++ scripts/sync_rclone.sh
@@ -5,5 +5,11 @@ FOLDERS=(Pictures Videos Documents Public)
 
 for FOLDER in "${FOLDERS[@]}"; do
 	echo "Synchronizing $FOLDER..."
-	rclone sync $HOME/$FOLDER $REMOTE_NAME:$FOLDER -P
+	
+	if [[ -f "$HOME/$FOLDER/.rcloneignore" ]]; then
+		rclone sync $HOME/$FOLDER $REMOTE_NAME:$FOLDER -P --exclude-from $HOME/$FOLDER/.rcloneignore --verbose
+	else
+		rclone sync $HOME/$FOLDER $REMOTE_NAME:$FOLDER -P --verbose
+	fi
+
 done
