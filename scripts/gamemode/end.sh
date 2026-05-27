ea22c5de74f1f18667b26080f4fd5f07759c9827
thiagopls1 <thiagopls1@hotmail.com>
2026-05-26 22:16:18 -0300

    scripts: update gamemode scripts

--- scripts/gamemode/end.sh
+++ scripts/gamemode/end.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-hyprctl keyword decoration:blur:enabled true &
-hyprctl keyword animations:enabled true &
+hyprctl eval 'hl.config({ animations = { enabled = true } })' &
+hyprctl eval 'hl.config({ blur = { enabled = true } })' &
 qs -c noctalia-shell ipc call powerProfile disableNoctaliaPerformance &
 qs -c noctalia-shell ipc call idleInhibitor disable &
 scxctl stop &
--- scripts/gamemode/start.sh
+++ scripts/gamemode/start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-hyprctl keyword decoration:blur:enabled false &
-hyprctl keyword animations:enabled false &
+hyprctl eval 'hl.config({ animations = { enabled = false } })' &
+hyprctl eval 'hl.config({ blur = { enabled = false } })' &
+qs -c noctalia-shell ipc call powerProfile disableNoctaliaPerformance &
 qs -c noctalia-shell ipc call powerProfile enableNoctaliaPerformance &
 qs -c noctalia-shell ipc call idleInhibitor enable &
 scxctl start --sched lavd --mode gaming &
