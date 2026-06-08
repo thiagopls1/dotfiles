c701162986b697822e84f2532067e40acaeb15b1
thiagopls1 <thiagopls1@hotmail.com>
2026-06-08 08:55:46 -0300

    scripts: fix outdated hyprctl commands

--- scripts/gamemode/end.sh
+++ scripts/gamemode/end.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 hyprctl eval 'hl.config({ animations = { enabled = true } })' &
-hyprctl eval 'hl.config({ blur = { enabled = true } })' &
+hyprctl eval 'hl.config({ decoration = { blur = { enabled = true } } })' &
 qs -c noctalia-shell ipc call powerProfile disableNoctaliaPerformance &
 qs -c noctalia-shell ipc call idleInhibitor disable &
 scxctl stop &
--- scripts/gamemode/start.sh
+++ scripts/gamemode/start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 hyprctl eval 'hl.config({ animations = { enabled = false } })' &
-hyprctl eval 'hl.config({ blur = { enabled = false } })' &
-qs -c noctalia-shell ipc call powerProfile disableNoctaliaPerformance &
+hyprctl eval 'hl.config({ decoration = { blur = { enabled = false } } })' &
 qs -c noctalia-shell ipc call powerProfile enableNoctaliaPerformance &
 qs -c noctalia-shell ipc call idleInhibitor enable &
 scxctl start --sched lavd --mode gaming &
